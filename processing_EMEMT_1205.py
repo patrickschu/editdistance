323d32bfbca4d1729b85a6b4da1559f7f1cdcc3f
patrick <patrickschultz@utexas.edu>
2016-12-05 20:08:51 -0600

    this was needed to merge files and save them to one place"

--- processing_EMEMT_1205.py
+++ processing_EMEMT_1205.py
@@ -27,17 +27,19 @@ def mixer(input_dir, list_of_endings):
 				#print re.sub("txt|html", "XXX", "fili.html")
 				filidict[re.sub("("+"|".join(list_of_endings)+")", "", fili)].append(os.path.join(input_dir, folder, fili))
 	for k in [i for i in filidict.keys() if i not in ["1687_Barbette_ThesaurusChirurgiae", "1655_Hartlib_ChymicalAddresses", "1640_Bartlet_WarmingStone"]]:
-		if len(filidict[k]) < 2:
-			print k, filidict[k]
+		print k
 				
-		#print "workin on", filidict[k][0]
+		print "workin on", filidict[k][0][-4:]
 		#so the text is in latin-1 and the html in utf-8?? how messed up is that?
-		#with codecs.open(filidict[k][0], "r", "latin-1") as input_1:
-		#	input_1=input_1.read()
-			#print input_1[:200]
-# 		print "workin on", filidict[k][1]
-#  		with codecs.open(filidict[k][1], "r", "utf-8") as input_2:
-#  			input_2=input_2.read()
+		#note that below relies on the fact that all the txt are first in dict entry
+		with codecs.open(filidict[k][0], "r", "latin-1") as input_1:
+			input_1=input_1.read()
+		print "workin on", filidict[k][1][-4:]
+ 		with codecs.open(filidict[k][1], "r", "utf-8") as input_2:
+ 			input_2=input_2.read()
+ 		with codecs.open(os.path.join("outputfiles", k+"_consolidated.txt"), "w", "utf-8") as output:
+ 			output.write("<text>"+input_1+"</text>--------------<html>"+input_2+"</html>")
+ 		print "written to:", os.path.join("outputfiles", k+"_consolidated.txt")
 		
 #is there metadata for 1687_Barbette_ThesaurusChirurgiae.txt. no, that is not a thing
 # not text on the other hand for 1655_Hartlib_ChymicalAddresses_info.html
