61e9e348eeeef7acc4672f40edc8758b7f488229
patrick <patrickschultz@utexas.edu>
2016-12-06 14:30:32 -0600

    more exploration

--- explore_dataset_1205.py
+++ explore_dataset_1205.py
@@ -18,20 +18,22 @@ def tagextractor(text, tag, fili):
 
 
 
-def taggetter(input_dir, tag):
+def explorer(input_dir):
 	for fili in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
 		print "\n\n", fili
-		with codecs.open(os.path.join(input_dir, fili), "r", "utf-8") as inputfili:
-			inputtext=inputfili.read()
-			#print "raw text", inputtext[-100:]
-			result=emo.tagextractor(inputtext, tag, fili)
-			print result
-			
-#taggetter("/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted/", "author")
-
-rr=emo.CorpusText("/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted/L_STUART_058_extracted.txt")
+		text=emo.CorpusText(os.path.join(input_dir, fili))
+		for item in text.meta:
+			print item, text.meta[item]
+explorer ('/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted/')
+#rr=emo.CorpusText("/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted/L_STUART_058_extracted.txt")
 
 
-rr.test()
 
-rr.getdetail("notes")
+#rr.test()
+
+#rr.getdetail("notes")
+
+# for k in rr.meta:
+# 	print k, rr.meta[k]
+
+#print rr.meta['title']
