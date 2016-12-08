a96d33e4cf15204abc54fd874ef22881e3c46d0b
patrick <patrickschultz@utexas.edu>
2016-12-08 15:56:58 -0600

    add aggregator and subcorpus maker

--- explore_dataset_1205.py
+++ explore_dataset_1205.py
@@ -1,6 +1,7 @@
 import codecs
 import os
 import emodcorpustools as emo
+from collections import defaultdict
 
 ##INSPECTING
 
@@ -19,12 +20,46 @@ def tagextractor(text, tag, fili):
 
 
 def explorer(input_dir):
+	dicti=defaultdict(dict)
 	for fili in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
 		print "\n\n", fili
-		text=emo.CorpusText(os.path.join(input_dir, fili))
-		for item in text.meta:
-			print item, text.meta[item]
-explorer ('/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted/')
+		texti=emo.CorpusText(os.path.join(input_dir, fili))
+		dicti[fili]['wordcount']=texti.wordcount
+		dicti[fili]['charcount']=texti.charcount
+		dicti[fili]['avg_wordlength']=(dicti[fili]['charcount'])/(dicti[fili]['wordcount'])
+		for key in texti.meta:
+			dicti[fili][key]=texti.meta[key]
+
+	return dicti
+
+
+#for key in meta: fili[key]=meta[key]
+#dicti={fili:text.meta}
+#add word and character length
+
+fullcorpus=explorer ('/Users/ps22344/Desktop/innsbruck_extracted')
+
+
+def aggregator(dictionary, category, *list_of_terms):
+	dicti=defaultdict(dict)
+	for term in list_of_terms:
+		dicti[term]={k:v for k,v in dictionary.items() if term in dictionary[k][category]}
+		return dicti
+		
+
+	
+
+for key in fullcorpus:
+	print fullcorpus[key]['avg_wordlength']
+
+
+for corpus in set([v['corpus'] for k,v in fullcorpus.items()]):
+	by_subcorpus_corpus=aggregator(fullcorpus, 'corpus', corpus)
+	by_subcorpus_corpus=aggregator(fullcorpus, 'author', 'Mary Evelyn')
+	print by_subcorpus_corpus
+	
+#for corpus in set(fullcorpus)
+
 #rr=emo.CorpusText("/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted/L_STUART_058_extracted.txt")
 
 
