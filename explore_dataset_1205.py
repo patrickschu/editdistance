87bd86012771104259470e1df11312c7370a0a9d
patrick <patrickschultz@utexas.edu>
2016-12-10 08:38:38 -0600

    by author output

--- explore_dataset_1205.py
+++ explore_dataset_1205.py
@@ -2,6 +2,7 @@ import codecs
 import os
 import emodcorpustools as emo
 from collections import defaultdict
+import pandas
 
 ##INSPECTING
 
@@ -10,50 +11,58 @@ from collections import defaultdict
 def explorer(input_dir):
 	"""
 	The explorer wanders thru the input_dir, returns a dictionary with relevant info.
-	Info specified by CorpusText object.
+	Info supplied by CorpusText object.
 	"""
 	dicti=defaultdict(dict)
 	for fili in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
-		print "\n\n", fili
+		#print "\n\n", fili
 		texti=emo.CorpusText(os.path.join(input_dir, fili))
 		dicti[fili]['wordcount']=texti.wordcount
 		dicti[fili]['charcount']=texti.charcount
 		dicti[fili]['avg_wordlength']=(dicti[fili]['charcount'])/(dicti[fili]['wordcount'])
 		for key in texti.meta:
 			dicti[fili][key]=texti.meta[key]
-
 	return dicti
 
-
-#for key in meta: fili[key]=meta[key]
-#dicti={fili:text.meta}
-#add word and character length
-
-fullcorpus=explorer ('/Users/ps22344/Desktop/innsbruck_extracted')
-
-
-def aggregator(dictionary, category, *list_of_terms):
+def aggregator(dictionary, category, list_of_terms):
 	"""
 	The aggregator aggregates all items from dictionary.
 	It goes thru the list of terms, and returns a dictionary.
-	It add each term as a key and enters all the relevant data from dicitonary.
+	It adds each term as a key and enters all the relevant data from dicitonary.
+	category author and DKA in list of terms gets you
+	Dame Katherine Arundell  {'57_innsbruck_extracted.txt': {'genre1':, etc.
+	Note how we can just run this over a set() of authors contained in fullcorpus. 
 	"""
 	dicti=defaultdict(dict)
 	for term in list_of_terms:
+		print term
 		dicti[term]={k:v for k,v in dictionary.items() if term in dictionary[k][category]}
-		return dicti
+	print len(dicti)
+	return dicti
+
+
+
+filename="innsbruck_letters"
+#make full corpus
+fullcorpus=explorer ('/Users/ps22344/Desktop/innsbruck_extracted')
+print set([v['author'] for k,v in fullcorpus.items()])
+#look at authors
+fullcorpus_by_author=aggregator(fullcorpus, 'author', set([v['author'] for k,v in fullcorpus.items()]))
+#print fullcorpus_by_author
+for key in fullcorpus_by_author:
+	with codecs.open(filename+"_by_author.csv", "a", "utf-8") as fullcorpus_by_author_out:
+		pandas.DataFrame(fullcorpus_by_author[key]).T.to_csv(fullcorpus_by_author_out)
 		
 
 	
 
-for key in fullcorpus:
-	print fullcorpus[key]['avg_wordlength']
 
 
-for corpus in set([v['corpus'] for k,v in fullcorpus.items()]):
-	by_subcorpus_corpus=aggregator(fullcorpus, 'corpus', corpus)
-	by_subcorpus_corpus=aggregator(fullcorpus, 'author', 'Mary Evelyn')
-	print by_subcorpus_corpus
+
+# for corpus in set([v['corpus'] for k,v in fullcorpus.items()]):
+# 	by_subcorpus_corpus=aggregator(fullcorpus, 'corpus', corpus)
+# 	by_subcorpus_corpus=aggregator(fullcorpus, 'author', 'Mary Evelyn')
+# 	print by_subcorpus_corpus
 	
 #for corpus in set(fullcorpus)
 
