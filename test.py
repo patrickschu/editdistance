0050cda65a3162e29c414df73c28ad0a9944338c
U-laptop\ps <patrickschultz@utexas.edu>
2017-01-13 18:30:25 +0100

    we use this for basic corpus stats output

--- test.py
+++ test.py
@@ -1,34 +1,8 @@
-import re
+import emodcorpustools as emo
+import os
 
-
-print re.sub("txt|html", "XXX", "fili.html")
-
-meta=[
-("no",'X'), 
-("corpusnumber",'<L_(.*?_\d{3})>') , #<L_CONWAY_055>
-("corpus", "parsed_corpus_of_early_english_correspondence"), 
-("title",   '\{(?:ED|COM):(.*?)\}'), #<Q_\w{3}_.*?>
-("author", r"<A_(.*?)>"), #<p><strong>Author:</strong> Hieronymus Braunschweig [Brunschwig; Jerome of Brunswick] </p>
-("dialect", "bre"),
-("authorage", '<A-DOB_(\d{4})>'), # <A-DOB_1602>, '<A-DOB_(\d{4})>
-("pubdate", ':(?:E|M)\d:(\d{4})'), #E2:1593:
-("genre1", 'letter'), 
-("genre2", 'X'),
-("notes", 'The conventions used to indicate editorial comments and other types of text markup are the same as used in the ../annotation/intro.htm#text_markup" PPCME2/PPCEME'),
-("extraction_notes", """removed lines beginning with { or <; left all the capitalized comments in"""),
-("encoding", 'utf-8'),
-('text', "X")
-]
-
-
-m=[i[0] for i in meta]
-print m
-
-
-tt="Here x we go my x <xfriend<x>"
-
-regex=re.compile("(x)")
-
-#print [i if i else "xx" for i in regex.findall(tt)]
-
-print regex.search(tt).group()
+for spread in os.listdir("spread"):
+	t= emo.Corpus(os.path.join("spread", spread))
+	wordcount, worddict = t.wordcount('wordcount')
+	print "\n***\nThe corpus named '{}' contains {} files for a total of {} words.".format(t.corpusname, t.filecount(), wordcount)
+	print "{} different authors".format(len(t.categoryfeatures('author')['levels']))
