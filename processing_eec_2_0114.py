31155b0fd4d83c5c495ab0898e170e14e8277a4e
patrick <patrickschultz@utexas.edu>
2017-01-14 13:53:50 -0600

    this is what we used to produce the new version of extracted corpora

--- processing_eec_2_0114.py
+++ processing_eec_2_0114.py
@@ -11,7 +11,7 @@ while were at it, replace \n in metadata
 annotationregex=re.compile("\n(AUTHOR|LETTER|RECIPIENT)(:.+){5}.*")
 smallannotationregex=re.compile("[A-Z]*\,(\d+.){3}")
 metaregex=re.compile("<file>.*?<text>", re.DOTALL)
-textregex=re.compile("<text>.*?</text>", re.DOTALL)
+textregex=re.compile("<text>(.*?)</text>", re.DOTALL)
 
 def metacleaner (input_dir):
 	for fili in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
@@ -21,10 +21,14 @@ def metacleaner (input_dir):
 		meta=metaregex.findall(inputtext)[0]
 		#if len(meta) != 1:
 		#	print fili, "watch out, meta too long or short: it is {}".format(len(meta))
-		text=textregex.findall(inputtext)[0]
+		text= textregex.findall(inputtext)[0]
 		if len(text) != 1:
 			print fili, "watch out, text too long"
-		print annotationregex.sub(" ", text)
+		text= annotationregex.sub(" ", text)
+		text= smallannotationregex.sub("", text)
+		with codecs.open(os.path.join("eecprocessed", fili), "w", "utf-8") as outputfile:
+			outputfile.write(meta+text+"</text> </file>")
+		print "written to", outputfile
 			
 
 metacleaner('/Users/ps22344/Downloads/editdistance/extracted_corpora/eec_extracted')
