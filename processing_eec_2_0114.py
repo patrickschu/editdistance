4e700879009ba7a077ca6fed2a1343f2dca43500
patrick <patrickschultz@utexas.edu>
2017-02-24 12:11:54 -0600

    fix removal of caps names, make number deleteon more flexible

--- processing_eec_2_0114.py
+++ processing_eec_2_0114.py
@@ -4,25 +4,30 @@ import codecs
 import os
 
 """
+THIS IS IN ADDITION TO ~/Downloads/editdistance/processing_EEC_1206.py, NOT REPLACING IT
+It goes like this
+1.~/Downloads/editdistance/processing_EEC_1206.py
+2. Extracting text
+3. processing_eec_2_0114 (self)
 delete all the terrible annotations from eec
 while were at it, replace \n in metadata
 """
 
 annotationregex=re.compile("\n(AUTHOR|LETTER|RECIPIENT)(:.+){5}.*")
-smallannotationregex=re.compile("[A-Z]*\,(\d+.){3}")
+smallannotationregex=re.compile("[A-Z]*\,([A-Z]{,2}\,)?(\d+\.?){3}")
 metaregex=re.compile("<file>.*?<text>", re.DOTALL)
 textregex=re.compile("<text>(.*?)</text>", re.DOTALL)
 
 def metacleaner (input_dir):
 	for fili in [i for i in os.listdir(input_dir) if not i.startswith(".")]:
-		#print fili
+		print fili
 		with codecs.open(os.path.join(input_dir, fili), "r", "utf-8") as inputfile:
 			inputtext = inputfile.read()
 		meta=metaregex.findall(inputtext)[0]
 		#if len(meta) != 1:
 		#	print fili, "watch out, meta too long or short: it is {}".format(len(meta))
 		text= textregex.findall(inputtext)[0]
-		if len(text) != 1:
+		if len(textregex.findall(inputtext)) != 1:
 			print fili, "watch out, text too long"
 		text= annotationregex.sub(" ", text)
 		text= smallannotationregex.sub("", text)
@@ -31,7 +36,7 @@ def metacleaner (input_dir):
 		print "written to", outputfile
 			
 
-metacleaner('/Users/ps22344/Downloads/editdistance/extracted_corpora/eec_extracted')
+metacleaner('/Users/ps22344/Downloads/editdistance/outputfiles/eec_extracted')
 
 
 
