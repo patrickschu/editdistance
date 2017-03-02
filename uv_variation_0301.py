8e04082daa29725777d96556f3bc5607d12cdc0f
patrick <patrickschultz@utexas.edu>
2017-03-01 19:52:33 -0600

    in the making

--- uv_variation_0301.py
+++ uv_variation_0301.py
@@ -1,11 +1,23 @@
 import emodcorpustools as emod
 import re
+import json
+import codecs
+from string import maketrans
 
 
-dicti=emod.dictbuilder('/Users/ps22344/Desktop/extracted_corpora_02241')
+#dicti=emod.dictbuilder('/Users/ps22344/Desktop/extracted_corpora_02241', output_csv=True)
 
-print len(dicti)
 
+
+with codecs.open("dictbuilder_output.json", "r", "utf-8") as inputjson:
+	dicti = json.load(inputjson)
+
+print "lenght dicti", len(dicti)
+
+
+tab = maketrans("u", "v")
+for k in dicti.keys():
+	print k,k.encode("utf-8").translate(tab)
 
 #check out context
 def variantfinder(input_dict, variant_one, variant_two):
@@ -14,9 +26,14 @@ def variantfinder(input_dict, variant_one, variant_two):
 	I.e. if variant_one is "u" and variant_two is "v", this will pick up on "us" and "vs"
 	"""
 	print "running the variantfinder"
-	tab = maketrans(variant_one, variant_two)
-	outputdict= {k:v for k,v in input_dict if input_dict.get(k.translate(tab), None)} 
-	
+	variantonedict= {k:v for k,v in input_dict.items() if variant_one in list(k)}
+	outputdict= {}
+	for entry in variantonedict:
+		if input_dict.get(re.sub(variant_one, variant_two, entry), None):
+			outputdict[entry]={variant_one:variantonedict[entry], variant_two:input_dict[re.sub(variant_one, variant_two, entry)]}
+	for i in outputdict:
+		print i, outputdict[i]
+			
 	
 variantfinder(dicti, "u", "v")	
 	
