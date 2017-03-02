4dc6ebccb794ad233ec7a6f739aa38a1fa41ac5c
patrick <patrickschultz@utexas.edu>
2017-03-02 10:10:16 -0600

    finish and make shell runnable

--- uv_variation_0301.py
+++ uv_variation_0301.py
@@ -1,41 +1,77 @@
+#!/usr/bin/env python
+
 import emodcorpustools as emod
 import re
 import json
 import codecs
-from string import maketrans
+from collections import defaultdict
+import sys
 
-
+#building a dict
 #dicti=emod.dictbuilder('/Users/ps22344/Desktop/extracted_corpora_02241', output_csv=True)
 
-
-
-with codecs.open("dictbuilder_output.json", "r", "utf-8") as inputjson:
-	dicti = json.load(inputjson)
-
-print "lenght dicti", len(dicti)
-
-#check out context
-def variantfinder(input_dict, variant_one, variant_two):
-	"""
-	The variantfinder identifies words in in the input_dict that exist with both variant_1 and variant_2.
-	I.e. if variant_one is "u" and variant_two is "v", this will pick up on "us" and "vs"
-	"""
-	print "running the variantfinder"
-	variantonedict= {k:v for k,v in input_dict.items() if variant_one in list(k)}
-	outputdict= {}
-	for entry in variantonedict:
-		if input_dict.get(re.sub(variant_one, variant_two, entry), None):
-			outputdict[entry]={variant_one:variantonedict[entry], variant_two:input_dict[re.sub(variant_one, variant_two, entry)]}
-	for i in outputdict:
-		print i, outputdict[i]
-			
 	
-variantfinder(dicti, "u", "v")	
-	
-def contextfinder(input_dict, context_window):
+
+def main(variant_one, variant_two, input_dict, context_window):
 	"""
-	The contextfinder extracts all context of length context_window.
-	Returns a dict with counts for all of them.
+	Takes the two variants plus an input_dict (json file) with the vocab to go over.
 	"""
+	context_window=int(context_window)
 	
+	with codecs.open(input_dict, "r", "utf-8") as inputjson:
+		dicti = json.load(inputjson)
+	print "length dicti", len(dicti)
+
+	#find all variants
+	variantdict= emod.variantfinder(dicti, variant_one, variant_two)	
+
+	#establish their context, compile in dict
+	contextdict=defaultdict(dict)
+	for entry in variantdict:
+		for g in emod.contextfinder(entry, variant_one, context_window):
+			if not contextdict.get(g, None):
+				contextdict[g]={
+				variant_one: variantdict[entry][variant_one],
+				variant_two: variantdict[entry][variant_two],
+				'count': variantdict[entry][variant_one]+variantdict[entry][variant_two]
+				}
+
+	total= float(sum([v['count'] for v in contextdict.values()]))
+	print "contextdict has {} different contexts, with a total of {} tokens".format(len(contextdict), total)
+
+	#print most frequent contexts
+	#how they split up over variants 	
+	header="\n****\n"
+
+	print header, "MOST FREQUENT CONTEXTS of {} - {} variation (token counts < 10 excluded)".format(variant_one, variant_two), header
+	for item in sorted({k:v for k,v in contextdict.items() if v['count'] > 9}, key= lambda x : contextdict[x]['count'], reverse=True):
+		print "{context}\ttokens: {count}\t{percentoverall:.4}% of total,{var1}: {percentvar1:.4}%, {var2}: {percentvar2:.4}%".format(
+		context= "_".join("".join(i) for i in item).encode('utf-8'), 
+		count= contextdict[item]['count'], 
+		percentoverall= contextdict[item]['count']/total*100,
+		percentvar1= contextdict[item][variant_one]/float(contextdict[item]['count'])*100, 
+		var1=variant_one.encode('utf-8'), 
+		percentvar2= contextdict[item][variant_two]/float(contextdict[item]['count'])*100, 
+		var2= variant_two.encode('utf-8')) 
+	
+	#print most one-sided contexts
+	#how they split up over variants 
+	print header, "HIGHEST DIFFERENTIAL IN VARIANT USE of {} - {} variation (token counts < 10 excluded)".format(variant_one, variant_two)		
+	for item in sorted({k:v for k,v in contextdict.items() if v['count'] > 9}, key= lambda x : abs(contextdict[x][variant_one]/float(contextdict[x]['count']) - contextdict[x][variant_two]/float(contextdict[x]['count'])), reverse=True):
+		print "{context}\ttokens: {count}\t{percentoverall:.4}% of total,{var1}: {percentvar1:.4}%, {var2}: {percentvar2:.4}%".format(
+		context= "_".join("".join(i) for i in item).encode('utf-8'), 
+		count= contextdict[item]['count'], 
+		percentoverall= contextdict[item]['count']/total*100,
+		percentvar1= contextdict[item][variant_one]/float(contextdict[item]['count'])*100, 
+		var1=variant_one.encode('utf-8'), 
+		percentvar2= contextdict[item][variant_two]/float(contextdict[item]['count'])*100, 
+		var2= variant_two.encode('utf-8')) 
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
+    
+    
+    
+#main("u", "v", 'dictbuilder_output.json')
 
