d2f069705942728c256341a040c9cdd3419e15de
patrick <patrickschultz@utexas.edu>
2017-04-07 10:45:52 -0500

    make shell runnable; add dicts for counts and output

--- analysis_scripts/editdist_shell.py
+++ analysis_scripts/editdist_shell.py
@@ -1,11 +1,13 @@
 import nltk
 from nltk import edit_distance
+from collections import defaultdict
 import codecs
 import os
 import re
 import sys
 
 
+header= "\n\n***\n"
 
 def spellfinder(word, text, distance):
 	"""
@@ -15,32 +17,49 @@ def spellfinder(word, text, distance):
 	"""
 	print "We have {} words in the text, distance is set to {} for '{}'".format(len(text), distance, word)
 	#print text
-	result=[i for i in text if nltk.edit_distance(word, i) <= distance]
-	print result
-	print len(result)
+	result=[i for i in text if (nltk.edit_distance(word, i) <= distance) and (nltk.edit_distance(word, i) > 0)]
+	print "results", result
+	print "number of items", len(result)
 	if len(result) > 0:
-		result=set(result)
+		resultset=set(result)
 		#print "result: ", result
-		print "{} matches".format(len(list(result)))
+		print "{} matches".format(len(list(resultset)))
+	return result
 		
 	
 
-def main(wordlist, data_set, distance):
+def main(wordlist, input_dir, distance):
+	countdict=defaultdict(float)
+	worddict=defaultdict(list)
 	#reading in the wordlist
 	with codecs.open(wordlist, "r", "utf-8") as wordlist:
 		wordlist=wordlist.read().split("\n")
 	#iterate over files in data_set
-	for fili in [i for i in os.listdir(data_set) if not i.startswith(".")]:
-		print "\n---\nWorking on ", fili
-		#catch encoding errors
-		try:
-			with codecs.open(os.path.join(data_set, fili), "r", "utf-8") as text:
- 				text=re.split("\W", text.read().lower())
- 				text=[i for i in text if i]
-			for w in wordlist: 
-				spellfinder(w, text, distance)
-		except UnicodeError, e:
-			print "WARNING: RETURNED ERROR:\n", e
+	for w in os.walk(input_dir):
+		folder=w[0]
+		files=w[2]
+ 		for fili in [i for i in files if i.endswith(".txt")]:
+			#catch encoding errors
+			try:
+				with codecs.open(os.path.join(folder, fili), "r", "utf-8") as text:
+					text=re.split("\W", text.read().lower())
+					text=[i for i in text if i]
+				for w in wordlist: 
+					variants= spellfinder(w, text, distance)
+					worddict[w]=worddict[w]+list(set(variants))
+					for v in variants:
+						countdict[v]=countdict[v]+1
+			except UnicodeError, e:
+				print "WARNING: RETURNED ERROR:\n", e
+	for w in wordlist:
+		print "{}{} Variants for '{}':".format(header,len(set(worddict[w])), w)
+		print ", ".join(sorted(list(set(worddict[w]))))
+	print "{}Token counts for variants:".format(header)
+	for v in sorted(countdict, key=countdict.get, reverse =True):
+		print v, countdict[v]
+	
+	#ATTN this will fail when two items share variants. fix later
+	
 			
 			
 #main('/Users/ps22344/Downloads/chapter2/current/wordlist.txt', "/Users/ps22344/Desktop/Lampeter_txt", 2)
