424c51c8d1ab77a59edb503e6b295526648c2ff6
patrick <patrickschultz@utexas.edu>
2017-04-20 17:42:35 -0500

    working version used to extract first round of csvs

--- analysis_scripts/variation_by_meta_0418.py
+++ analysis_scripts/variation_by_meta_0418.py
@@ -7,7 +7,7 @@ import os
 import emodcorpustools as emod
 import re
 import time
-
+import pandas
 
 
 
@@ -15,11 +15,12 @@ import time
 	
 	
 	
+input_dir= '/Users/ps22344/Downloads/extracted_corpora_0224'
+output_file= "helsinki"
 
-
-t=emod.dictbuilder_2('/Users/ps22344/Downloads/extracted_corpora_0224', 'pubdate')
+t=emod.dictbuilder_2(input_dir, 'pubdate')
 u, totaldict=emod.variantfinder_2(t, 'meta_data placeholder', 'u','v')
-print u.values()
+
 
 #reduce to 4 numbers
 #deal with missing
@@ -39,6 +40,18 @@ for year in u:
 	us= [[v for k,v in h.items() if k == 'u'] for h in u[year].values()]
 	usum= sum([image for menuitem in us for image in menuitem])
 	vs= [[v for k,v in h.items() if k == 'v'] for h in u[year].values()]
-	yeardict[year]= {'u':usum, 'v':vsum, 'types':len(u[year].keys())}
+	vsum= sum([image for menuitem in vs for image in menuitem])
+	yeardict[year]= {'u':usum, 'v':vsum, 'types':len(u[year].keys()), 'totalwords': totaldict[year]}
 
-print yeardict
+df=pandas.DataFrame.from_dict(yeardict, orient='index')
+
+print df.columns
+
+print list(df.columns)
+
+print len(list(df.columns))
+with codecs.open(output_file+".csv", "w") as csvout:
+	df.to_csv(csvout, index_label= 'year', encoding="utf-8")
+
+#print yeardict
+print "written to", output_file
