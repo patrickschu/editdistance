93b9a76d9deb89cbad735efea19aa258b6a40e0e
patrick <patrickschultz@utexas.edu>
2017-04-20 15:33:59 -0500

    add timer, add variantfinder_2, which extracts count by metadata cat

--- analysis_scripts/emodcorpustools.py
+++ analysis_scripts/emodcorpustools.py
@@ -25,6 +25,16 @@ metaregex= "({|<).*?(>|})"
 
 
 #helper functions
+def timer(func):
+	def wrapper(*args, **kwargs):
+		t = time.time()
+		res = func(*args, **kwargs)
+		print "{} took us {}".format(func.func_name, time.time()-t)
+		return res
+	return wrapper
+
+
+
 def authornameconverter(name_string, corpus_name):
 	"""
 	Converts all author names from different corpora to lastname_firstname according to rules associated with corpus_name. 
@@ -133,6 +143,41 @@ def contextfinder(input_word, variant, pre_window, post_window):
 		output=tuple(tuple((input_word[x] for x in i if -1 < x < len(input_word))) for i in ran)
 		#print output
 		yield output
+
+
+@timer
+def variantfinder_2(input_dict, meta_data, variant_one, variant_two):
+	"""
+	REMEMBER TO NICIFY THIS
+	The variantfinder_2 identifies words in in the input_dict that exist with both variant_1 and variant_2.
+	It builds on variantfinder but adds functionality to collect variants by external factors, such as time.
+	I.e. if variant_one is "u" and variant_two is "v", this will pick up on "us" and "vs".
+	It returns a dictionary with the counts for each variant, the key being variant_one.
+	"""
+	print "running the variantfinder_2"
+	
+	metadict = defaultdict(dict)
+	for entry in input_dict:
+		print entry
+		print input_dict[entry].keys()
+		for key in input_dict[entry].keys():
+			if not metadict[key].get(entry, None):
+				metadict[key][entry] = input_dict[entry][key]
+			else:
+				print "Alarm this is weird"
+	for key in metadict:
+		#add totals for each year
+		metadict[key]['total'] = sum(metadict[key].values())
+	variantonedict= {key:{k:v for k,v in val.items() if variant_one in list(k)} for key,val in metadict.items()}
+	#clean interior dictionary of non-varying items
+	combineddict= {key:{k:v for k,v in val.items() if re.sub(variant_one, variant_two, k) in input_dict} for key,val in variantonedict.items()}
+	outputdict= {}
+	for entry in combineddict:
+		#entry is a year
+		print combineddict[entry]
+		#format is {year1:{word1:{variant_one:X, variant_two:X}, word2:{}}, year2:{}}
+		outputdict[entry]={k:{variant_one:v, variant_two:metadict[entry].get(re.sub(variant_one, variant_two, k), 0)} for k,v in combineddict[entry].items()}
+	return outputdict
 
 
 
