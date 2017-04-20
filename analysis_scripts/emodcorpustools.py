ee27f801886f8d3e81a78633fe75933616e91280
patrick <patrickschultz@utexas.edu>
2017-04-20 16:46:23 -0500

    add output of totaldict

--- analysis_scripts/emodcorpustools.py
+++ analysis_scripts/emodcorpustools.py
@@ -96,7 +96,7 @@ def dictbuilder_2(input_dir, meta_data, output_json=False):
 		print "folder", folder 
 		for fili in [i for i in w[2] if i.endswith(".txt")]:
 			text= CorpusText(os.path.join(input_dir, folder, fili))
-			if meta_data='pubdate':
+			if meta_data == 'pubdate':
 				meta= text.meta[meta_data][:4]
 			else:
 				meta= text.meta[meta_data]
@@ -156,9 +156,9 @@ def variantfinder_2(input_dict, meta_data, variant_one, variant_two):
 	It builds on variantfinder but adds functionality to collect variants by external factors, such as time.
 	I.e. if variant_one is "u" and variant_two is "v", this will pick up on "us" and "vs".
 	It returns a dictionary with the counts for each variant, the key being variant_one.
+	Totaldict gives word count by meta_data category
 	"""
 	print "running the variantfinder_2"
-	
 	metadict = defaultdict(dict)
 	for entry in input_dict:
 		print entry
@@ -168,19 +168,20 @@ def variantfinder_2(input_dict, meta_data, variant_one, variant_two):
 				metadict[key][entry] = input_dict[entry][key]
 			else:
 				print "Alarm this is weird"
+	totaldict= defaultdict(int)
 	for key in metadict:
 		#add totals for each year
-		metadict[key]['total'] = sum(metadict[key].values())
+		totaldict[key] = sum(metadict[key].values())
 	variantonedict= {key:{k:v for k,v in val.items() if variant_one in list(k)} for key,val in metadict.items()}
 	#clean interior dictionary of non-varying items
-	combineddict= {key:{k:v for k,v in val.items() if re.sub(variant_one, variant_two, k) in input_dict or k in ['total']} for key,val in variantonedict.items()}
+	combineddict= {key:{k:v for k,v in val.items() if re.sub(variant_one, variant_two, k) in input_dict} for key,val in variantonedict.items()}
 	outputdict= {}
 	for entry in combineddict:
 		#entry is a year
 		print combineddict[entry]
 		#format is {year1:{word1:{variant_one:X, variant_two:X}, word2:{}}, year2:{}}
 		outputdict[entry]={k:{variant_one:v, variant_two:metadict[entry].get(re.sub(variant_one, variant_two, k), 0)} for k,v in combineddict[entry].items()}
-	return outputdict
+	return outputdict, totaldict
 
 
 
