5012bec897422da9d00ac6bf3c59ec5fe0701e48
patrick <patrickschultz@utexas.edu>
2017-04-20 11:23:17 -0500

    owkring on the new varianfinder

--- analysis_scripts/variation_by_meta_0418.py
+++ analysis_scripts/variation_by_meta_0418.py
@@ -5,6 +5,7 @@ import codecs
 import json
 import os
 import emodcorpustools as emod
+import re
 
 def variantfinder_2(input_dict, variant_one, variant_two):
 	"""
@@ -15,6 +16,7 @@ def variantfinder_2(input_dict, variant_one, variant_two):
 	"""
 	print "running the variantfinder"
 	variantonedict= {k:v for k,v in input_dict.items() if variant_one in list(k)}
+	print variantonedict
 	outputdict= {}
 	for entry in variantonedict:
 		if input_dict.get(re.sub(variant_one, variant_two, entry), None):
@@ -23,28 +25,8 @@ def variantfinder_2(input_dict, variant_one, variant_two):
 	
 	
 	
-def dictbuilder_2(input_dir, meta_data, output_json=False):
-	"""
-	Builds a dictionary of all texts in input_dir.
-	Builds on dictbuilder but adds functionality to collect by external factors such as date.
-	Format: {word: {year: count, year:count, year:count}, word: {}}
-	"""
-	dicti=defaultdict(dict)
-	for w in os.walk(input_dir):
-		folder=w[0]
-		print "folder", folder 
-		for fili in [i for i in w[2] if i.endswith(".txt")]:
-			text= emod.CorpusText(os.path.join(input_dir, folder, fili))
-			meta= text.meta[meta_data]
-			for word in text.tokenizer(cleantext=True):
-				#dicti[word.lower()]= dicti[word.lower()]+1
-				x=1
-	if output_json:
-		with codecs.open(output_json+".json", "w") as jsonout:
-			json.dump(dicti, jsonout, encoding= "utf-8")
-		print "File written to", jsonout
-	print "\n".join([":".join((i, str(dicti[i]))) for i in sorted(dicti, key= dicti.get, reverse=True)[:100]])
-	return dicti
-	
 
-dictbuilder_2('/Users/ps22344/Downloads/extracted_corpora_0224', 'pubdate')
+
+t=emod.dictbuilder_2('/Users/ps22344/Downloads/extracted_corpora_0224', 'pubdate')
+u=variantfinder_2(t, 'u','v')
+print u.values()
