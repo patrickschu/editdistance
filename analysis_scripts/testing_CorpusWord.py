f251942709f7d41005c94312bf026977a041ea6d
patrickschu <patrickschultz@utexas.edu>
2017-06-20 19:27:18 -0500

    testing the variantItem

--- analysis_scripts/testing_CorpusWord.py
+++ analysis_scripts/testing_CorpusWord.py
@@ -1,5 +1,5 @@
 import emodcorpustools as emod
-
+header = "\n\n+++\n\n"
 corpusdir = '/Users/ps22344/Downloads/extracted_corpora_0420'
 corpusdir = '/home/patrick/Downloads/editdistance/extracted_corpora_0420_small'
 searchterm = "duke"
@@ -18,7 +18,7 @@ def main(input_dir):
 	v = emod.dictbuilder(input_dir)
 	emod.findvariants(v, "u", "v")
 
-main(corpusdir)
+#main(corpusdir)
 	
 def main(input_dir):
 	t = "deppski"
@@ -29,4 +29,19 @@ def main(input_dir):
 
 #for word in ["uu", "sup", "suup", "uussuuu"]:
 	#emod.VariantItem(word).indexer("u")
-		
+
+from itertools import chain, combinations
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(set(iterable))  # doesn't allow duplicate elements
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    
+for g in powerset([1]):
+	print g
+	
+
+print header
+
+for g in powerset([1,2, 2, 3,4]):
+	print g
