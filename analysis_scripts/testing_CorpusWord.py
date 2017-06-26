0c933c74cf9b62990297845dbcb65a6a1f167eda
patrickschu <patrickschultz@utexas.edu>
2017-06-26 12:09:01 -0500

    adding threshold etc and clean up dictionary for output script in 'testing....py'

--- analysis_scripts/testing_CorpusWord.py
+++ analysis_scripts/testing_CorpusWord.py
@@ -12,22 +12,38 @@ def main(search_term, input_dir):
 	print dicti
 	print dicti.viewvalues()
 
-#test Dictbuilder
-@emod.timer
-def main(input_dir, variant_one, variant_two):
-	vocab = emod.Corpus_2(corpusdir).vocabbuilder(output_json = "testvocab")
+
+	#check if set deletes non-ID CorpusWords -- apparently it does not
+	
+#main(corpusdir, "u", "v")
+
+def main(input_dir, variant_one, variant_two, threshold = 0):
+	#vocab = emod.Corpus_2(corpusdir).vocabbuilder(output_json = "testvocab")
+	vocab = emod.CorpusVocabImporter('/home/patrick/Downloads/editdistance/testvocab.json')
 	print "len vocab ", len(vocab)
 	onedict = {k:v for k,v in vocab.viewitems() if variant_one in list(k)}
 	print "len onedict ", len(onedict)
+	# for each word, construct a VariantItem containing all possible types with variant_two
+	# this only returns items that are contained in the corpus vocab
+	# Resulting typedict looks like so: {CorpusWord : {position: CorpusWord, ..}}
 	onedict = {emod.VariantItem(k, variant_one, variant_two, input_vocab = vocab) : v for k,v in onedict.viewitems()}
-	print {k.word : k.typedict for k,v in onedict.viewitems() if k.typedict.values() != [set([])]}
-	#check if set deletes non-ID CorpusWords -- apparently it does not
-	print {k.word : k.typedict.values() for k,v in onedict.viewitems() if k.typedict.values() != [set([])]}
+	#We work with a dictionary like this: {VariantItem:CorpusWord, VariantItem:CorpusWord...} where CorpusWord is a representation of the original variant_one word
+	print "len onedict ", len(onedict)
+	#remove words with empty typedictionaries, i.e. that don't have any variant_2 tokens
+	onedict = {k:v for k,v in onedict.viewitems() if k.typedict.values()[0]}
+	print "len onedict ", len(onedict)
+	#this will give us the total tokens for each word with variant_one
+	# {v.word:v.totaltokens() for k,v in onedict.viewitems()}
+	onedict = {k:v for k,v in onedict.viewitems() if 
+	#filter for the ones above threshold
 	
+	#extract word counts over the years for each word over threshold
+	#extract variation counts by year
 	
-	#emod.findvariants(v, "u", "v")
 
+	
 main(corpusdir, "u", "v")
+
 	
 
 		
