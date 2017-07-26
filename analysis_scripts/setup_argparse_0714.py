5ac7f6cb04040a87cee5be5fd1e2b6408a4d3053
patrickschu <patrickschultz@utexas.edu>
2017-07-25 20:29:55 -0500

    this is the outline for the argpars functionality in variantfinder

--- analysis_scripts/setup_argparse_0714.py
+++ analysis_scripts/setup_argparse_0714.py
@@ -12,31 +12,22 @@ def main():
 	parser.add_argument("--read_corpusfile", type=str, 
 		help="Enter name of json file containing a file with wordcounts created previously")
 	parser.add_argument("--input_dir", type=str, help="Enter the directory containing corpus files")
-	parser.add_argument("--threshold", type=str, 
+	parser.add_argument("--threshold", type=int, 
 		help="OPTIONAL: Enter the minimum number of tokens varying between variant_one and variant_two that need to be present to be included in the output. Defaults to '0'")
-
-#	parser.add_argument("--timespan", type=tuple, help="Enter a start and end year for the output, seperated by a comma. Format: 'beginning,end'")
-#	parser.add_argument("output_words", type=str, help="Enter file name to write csv of token counts per word and position")
-#	parser.add_argument("output_aggregate_position", type=str, help="Enter file name to write csv of token counts per position")
-#	parser.add_argument("output_aggregate_total", type=str, help="Enter file name to write csv of token counts per variant and year")
-#	parser.add_argument("--write_corpusfile", type=str, help="Enter name of file to write a file with word counts per year, create from input_directory")
-
+	parser.add_argument("--timespan", type=str, help="Enter a start and end year for the output, seperated by a comma. Format: 'beginning,end'")
+	#output options
+	parser.add_argument("--output_words", type=str, help="Enter file name to write csv of token counts per word and position")
+	parser.add_argument("--output_aggregate_position", type=str, help="Enter file name to write csv of token counts per position")
+	parser.add_argument("--output_aggregate_total", type=str, help="Enter file name to write csv of token counts per variant and year")
+	parser.add_argument("--write_wordcount_to_file", type=str, help="Enter name of file to write a file with word counts per year, create from input_directory")
+	#read input
 	args = parser.parse_args()
 	#checking input
 	if all([not args.read_corpusfile, not args.input_dir]):
 		raise IOError("No input data. You need to either specify a corpus directory as '--input_dir' or a file with word counts as '--read_file'")
-	
-	if threshold:
-		threshold = threshold.split(",")
-	
-	
-	print args.read_corpusfile
-	print args.input_dir
-	
-	print type(args)
-	print vars(args)
+	if args.timespan:
+		# turns input into [str, str] list
+		timespan = args.timespan.split(",")
 	
 	
-if __name__ == "__main__":
-    main()
     
