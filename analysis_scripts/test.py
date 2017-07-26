0c3d002987da7404c26d200ccc6d0bac619f805b
patrickschu <patrickschultz@utexas.edu>
2017-07-25 20:30:28 -0500

    here we test things

--- analysis_scripts/test.py
+++ analysis_scripts/test.py
@@ -1,71 +1,67 @@
-import emodcorpustools as emo
-import os
-import sys
+import pandas
 
-# for spread in os.listdir("spread"):
-# 	t= emo.Corpus(os.path.join("spread", spread))
-# 	wordcount, worddict = t.wordcount('wordcount')
-# 	print "\n***\nThe corpus named '{}' contains {} files for a total of {} words.".format(t.corpusname, t.filecount(), wordcount)
-# 	print "{} different authors".format(len(t.categoryfeatures('author')['levels']))
+dd = {1600: 
+	{"ut_('base',)": 0, "ut_(1,)": 100, "ut_(0,1)": 100000, "uttt_(1,)": 333, "fu_(1,1)": 2323}, 
+	1601: 
+	{"ut_('base',)": 1, "ut_(1,)": 200, "ut_(0,1)": 200000, "uttt_(1,)": 333, "fu_(1,1)": 2323},
+	1603:
+	{"ut_('base',)": 2, "ut_(1,)": 300, "ut_(0,1)": 300000, "uttt_(1,)": 333, "fu_(1,1)": 2323}
+	}
+
+dd = {k:v for k,v in dd.viewitems() if not v in ["fu_(1,1)"]} 
+print dd.values()
+df = pandas.DataFrame(dd).T
+
+#splitcols = [i.split("_") for i in df.columns]	
+#print splitcols
+splitcols2 = [(w, eval(m)) for w,m in [i.split("_") for i in df.columns]]
+print [(type(x), type(z)) for x,z in splitcols2]
+print splitcols2
+df.columns = splitcols2
+
+print df
+
+column_filter = [(word, attr) for word, attr in df.columns if not attr[0]]
+column_filter = [attr for word, attr in df.columns]
+
+word, attr = ('ut', (1,))
+print "w", word
+print "a", attr
+
+def filti(x):
+	print "tt", type(x)
+	word, attr = x
+	print "w", word
+	print "a", attr
+	return attr.__hash__(), attr
+
+
+sel = df.columns.map(lambda x: filti(x))
+
+g= df.groupby(lambda x: filti(x), axis = 1).sum()
+g.columns = [i[1] for i in g.columns]
+
+print g
+
+#print g.columns
+print [type(i) for i in g.columns]
+
+#print "result", df[df.columns[sel]]
 
 
 
-# texti="meinei"
-# length=3
-# 
-# #extract context for "i"
-# #[(0, 'm'), (1, 'e'), (2, 'i'), (3, 'n'), (4, 'e')]
-# #we just have to run this over the control letter
-# #control and stimulus letter
-# 
-# letter="i"
-# 
-# 
-# 
-# print indices
-# 
-# ranges= [(range(i-length, i),range(i+1, i+1+length))  for i in indices]
-# 
-# print ranges
+# get by position
+# year, by position
+# sum all with ID position
+# "sum all columns where attr == attr, over rows
+result = pandas.DataFrame()
 
 
 
-# for r in ranges:
-# 	print r
-# 	#for each entry in the two tuples in r, match to texti
-# 	#r=[[1,2,3],[2,3,4]]
-# 	output=[tuple((texti[x] for x in i if -1 < x < len(texti))) for i in r]
+
+#get per year: base vs all
 
 
-# def contextfinder(input_word, variant, context_window=2):
-# 	"""
-# 	The contextfinder finds all instances of variant in the input_word.
-# 	It yields a number of characters preceding and folllowing it as specified in context_window.
-# 	"""
-# 	#establish position of variants in the word
-# 	indices= [no for no,i in enumerate(list(input_word)) if i==variant]
-# 	if len(indices) < 1:
-# 		print "\nERROR in contextfinder: No instances of '{}' found in '{}'\n".format(variant, input_word)
-# 	#establish the indices for context
-# 	ranges= [(range(i-context_window, i),range(i+1, i+1+context_window))  for i in indices]
-# 	for ran in ranges:
-# 		output=[tuple((input_word[x] for x in i if -1 < x < len(input_word))) for i in ran]
-# 		print output
-# 		yield output
-# 	
-# 	
-# 
-# for g in contextfinder("muhu", "v", 3):
-# 	print g
-# def main(a,b):
-# 	print "args are", a,b
-# 
-# if __name__ == '__main__':
-#     # main should return 0 for success, something else (usually 1) for error.
-#     print sys.argv[1:]
-#     main(*sys.argv[1:])
-    
-for g in emo.contextfinder("muschibear", "u", 100, 5):
-	print g
-#input_word, variant, pre_window, post_window)
+
+
 
