e3e7f547afef153f400bfd5e05d38a5a08310a90
patrickschu <patrickschultz@utexas.edu>
2017-07-29 14:35:36 -0500

    extract per year characteristics

--- analysis_scripts/testing_output_0729.R
+++ analysis_scripts/testing_output_0729.R
@@ -4,11 +4,12 @@ lampeter_inputdir =   '/home/patrick/Downloads/editdistance/analysis_scripts/inp
 supercorpus_uv = '/home/patrick/Downloads/editdistance/analysis_scripts/uvwords.csv'
 supercorpus_vu = '/home/patrick/Downloads/editdistance/analysis_scripts/vuwords.csv'
 
+step1rs = '/home/patrick/Downloads/editdistance/analysis_scripts/step1rswords.csv'
+step1sr = '/home/patrick/Downloads/editdistance/analysis_scripts/step1srwords.csv'
 
-
-sinki = TRUE
+sinki = FALSE
 spread = read.csv(
-  supercorpus_vu, 
+  step1sr, 
   header = TRUE,
   encoding = "UTF-8")
 
@@ -18,7 +19,9 @@ spread = read.csv(
 if (sinki == TRUE) {sink(file = 'vu_words.txt'); print ('start the sinking')}
 #get the format right, with index etc
 rownames(spread) = spread[['X']]
-spread[['X']] = NULL
+#print (class(spread[['X']]))
+#print (class(rownames(spread)))
+#spread[['X']] = NULL
 print(rownames(spread))
 cat ("Rows in here:", nrow(spread), "; Columns in here:", ncol(spread))
 cat("Total sum of counts is :", sum(colSums(spread, na.rm = TRUE)), "; mean is:", mean(colSums(spread, na.rm = TRUE)))
@@ -30,3 +33,8 @@ cat(colSums(spread[ordered][1:5], na.rm = TRUE))
 #print (colnames(spread))
 cat("Values per year", rowSums(spread[1:10,], na.rm = TRUE))
 if (sinki == TRUE) {sink(file = NULL); print("stopped the sinking")}
+
+#THIS IS RS
+#sink('sr_1571.txt')
+spread[, !is.na(spread['1571',])]['1571',]
+#sink()
---
+++ analysis_scripts/trash.R
@@ -0,0 +1,10 @@
+
+
+
+
+tf = data.frame("a" = c(1,2,NA,4), "b" = c(NA, 1000,NA,4000))
+tf
+rownames(tf) = c(1000,1571, 1572, 2000)
+tf
+tf[, !is.na(tf['1000',])]
+tf
